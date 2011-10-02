2532e4131d32dab64755962275adf586ef7e3f6d
Tim Fischbach <mail@timfischbach.de>
2011-10-02 11:09:02 +0200

    rewrite server to use connect

--- server/app.js
+++ server/app.js
@@ -1,42 +1,26 @@
-var http = require('http'),
-    io = require('socket.io'),
-    url = require('url'),
-    server, manager, target;
+/*global require, console*/
 
-function handleRequest(req, res) {
-  if (!target) {
-    res.writeHead(500, {'Content-Type': 'text/plain'});
-    res.end('No grooveshark instance connected\n');
-    return;
-  }
+var connect = require('connect'),
+    io, server;
 
-  var u = url.parse(req.url)
+server = connect(
+  connect.router(function(app) {
+    app.get('/handle/:handle/:command', function(req, res) {
+      console.log('emit ' + req.params.command + ' for ' + req.params.handle);
+      io.sockets.in(req.params.handle).emit(req.params.command);
 
-  if (u.pathname === '/playPause') {
-    target.emit('playPause');
-  }
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Dispatched');
+    });
+  })
+);
 
-  if (u.pathname === '/previous') {
-    target.emit('previous');
-  }
-
-  if (u.pathname === '/next') {
-    target.emit('next');
-  }
-
-  if (u.pathname === '/volume') {
-    target.emit('volume', u.query.value);
-  }
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Dispatched.');
-}
-
-server = http.createServer(handleRequest);
-server.listen(4000);
-
-manager = io.listen(server);
-
-manager.sockets.on('connection', function(socket) {
-  target = socket;
+io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket) {
+  socket.on('join', function(handle) {
+    console.log('joining ' + handle);
+    socket.join(handle);
+  });
 });
+
+server.listen(4000);
